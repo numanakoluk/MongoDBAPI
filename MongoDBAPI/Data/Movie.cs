3b31c15947c38b9c93d68629c710e6bb1222419a
numanakoluk <numanakoluk01@gmail.com>
2022-04-16 00:49:48 +0300

    Add project files.

---
+++ MongoDBAPI/Data/Movie.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDBAPI.Data
+{
+    public class Movie
+    {
+        //Serilaized
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Summary { get; set; } = null;
+        public List<string> Actors { get; set; }
+
+    }
+}
---
+++ MongoDBAPI/Data/MoviesDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace MongoDBAPI.Data
+{
+    public class MoviesDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+
+        public string DataBaseName { get; set; } = string.Empty;
+
+        public string MoviesCollectionName { get; set; } = string.Empty;
+    }
+}
